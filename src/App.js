a17b415a5c0ed31433094b86d560bc2604b7926c
evgen <gormoshka123@gmail.com>
2023-06-07 23:16:33 +0300

    Added Introdaciton and available meals components and rendered them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import MainHader from "./components/Hader/MainHader";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <MainHader />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from "./AvailableMeals";
+import Introduction from "./IntrodatctorySlogan";
+
+const Meals = (props) => {
+  return (
+    <>
+      <Introduction />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
