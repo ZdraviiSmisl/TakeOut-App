0ad90bed81946c4fc46c414e86c344a4e6e1a870
evgen <gormoshka123@gmail.com>
2023-06-15 22:28:06 +0300

    Added Cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,38 @@
+import React, { useReducer } from "react";
 import CartContext from "./CartContext";
 
+const defaultState = {
+  itmes: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    return {
+      items: state.items.concut(action.item),
+      totalAmount: state.totalAmount + action.item.price * action.item.amount,
+    };
+  }
+
+  return defaultState;
+};
+
 const CartProvider = (props) => {
-  const addItemHandler = (item) => {};
-  const removItmeHandler = (id) => {};
+  const [cartState, dispatchCartState] = useReducer(cartReducer, {
+    itmes: defaultState.itmes,
+    totalAmount: defaultState.totalAmount,
+  });
+
+  const addItemHandler = (item) => {
+    dispatchCartState({ type: "ADD", item: item });
+  };
+  const removItmeHandler = (id) => {
+    dispatchCartState({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.itmes,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removItmeHandler,
   };
