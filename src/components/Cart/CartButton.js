66761521ee70d152cff39336c92f4e0cccfb7a59
evgen <gormoshka123@gmail.com>
2023-06-20 19:45:05 +0300

    Added animation cart item using useEffect hook

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import styles from "./CartButton.module.css";
 import CartIcon from "./feature/CartImage";
 import CartContext from "../../store/CartContext";
 
 const CartButton = (props) => {
+  const [isAnimated, setIsAnimated] = useState(false);
   const context = useContext(CartContext);
   const itemNumber = context.items.reduce(
     (outputNum, item) => outputNum + item.amount,
     0
   );
 
+  const { items } = context;
+
+  const cartClasses = `${styles.button} ${!isAnimated ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setIsAnimated(true);
+
+    const tiemer = setTimeout(() => {
+      setIsAnimated(false);
+    }, 300);
+
+    return () => clearTimeout(tiemer);
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onOpen}>
+    <button className={cartClasses} onClick={props.onOpen}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
